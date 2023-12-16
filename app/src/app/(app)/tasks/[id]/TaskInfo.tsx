b488c9dbcf2567c6c61edbdcb94e3c8e9421a31d
prateeksonii <prateek_soni@outlook.com>
2023-12-16 20:50:25 +0530

    refactor ui

--- app/src/app/(app)/tasks/[id]/TaskInfo.tsx
+++ app/src/app/(app)/tasks/[id]/TaskInfo.tsx
@@ -27,6 +27,14 @@ import {
   CommandItem,
 } from "@/components/ui/command";
 import { cn } from "@/lib/utils";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { TypographyLead } from "@/components/typography";
 
 interface TaskInfoProps {
   task: typeof tasks.$inferSelect;
@@ -73,29 +81,6 @@ export default function TaskInfo(props: TaskInfoProps) {
     return () => clearTimeout(update);
   }, [task]);
 
-  const frameworks = [
-    {
-      value: "next.js",
-      label: "Next.js",
-    },
-    {
-      value: "sveltekit",
-      label: "SvelteKit",
-    },
-    {
-      value: "nuxt.js",
-      label: "Nuxt.js",
-    },
-    {
-      value: "remix",
-      label: "Remix",
-    },
-    {
-      value: "astro",
-      label: "Astro",
-    },
-  ];
-
   const createdByUser = props.users.find(
     (u) => u.users.id === task.createdBy
   )?.users;
@@ -103,127 +88,138 @@ export default function TaskInfo(props: TaskInfoProps) {
   return (
     <>
       <div className="grid grid-cols-[2fr_1fr] gap-8">
-        <div className="flex flex-col">
+        <Card className="flex flex-col">
+          <CardHeader>
+            <CardTitle>
+              <input
+                name="name"
+                className="bg-transparent w-full rounded-md focus-within:bg-stone-900 text-xl border-none px-2 py-1 text-stone-50 font-bold"
+                value={task.name ?? ""}
+                onChange={(e) => handleChange(e.target.value, "name")}
+              />
+            </CardTitle>
+            <CardDescription>
+              <textarea
+                className="bg-transparent rounded-md focus-within:bg-stone-900 w-full text-md mt-3 border-none p-2 text-stone-200"
+                value={task.description ?? ""}
+                placeholder="No description provided"
+                onChange={(e) => handleChange(e.target.value, "description")}
+              />
+            </CardDescription>
+          </CardHeader>
           <div
             className="ml-auto text-sm"
             dangerouslySetInnerHTML={{ __html: saveStatus }}
           ></div>
-          <input
-            name="name"
-            className="bg-transparent rounded-md focus-within:bg-stone-900 text-xl border-none px-2 py-1 text-stone-50 font-bold"
-            value={task.name ?? ""}
-            onChange={(e) => handleChange(e.target.value, "name")}
-          />
-          <textarea
-            className="bg-transparent rounded-md focus-within:bg-stone-900 text-md mt-3 border-none p-2 text-stone-200"
-            value={task.description ?? ""}
-            placeholder="No description provided"
-            onChange={(e) => handleChange(e.target.value, "description")}
-          />
-        </div>
+        </Card>
         <div className="flex flex-col gap-8">
-          <div className="p-8 bg-stone-900 rounded-xl grid grid-cols-2 gap-4">
-            <div className="flex flex-col">
-              <Label className="text-xs" htmlFor="budget">
-                Budget
-              </Label>
-              <Input
-                className="appearance-none [-moz-appearance:textfield]"
-                type="number"
-                id="budget"
-                onChange={(e) => handleChange(e.target.value, "budget")}
-                value={task.budget ?? 0}
-              />
-            </div>
-            <div className="flex flex-col">
-              <Label className="text-xs">Status</Label>
-              <Select
-                value={task.statusId?.toString()}
-                onValueChange={(v) => handleChange(v, "statusId")}
-              >
-                <SelectTrigger className="">
-                  <SelectValue placeholder="Status" />
-                </SelectTrigger>
-                <SelectContent>
-                  {props.statuses.map((status) => (
-                    <SelectItem key={status.id} value={status.id.toString()}>
-                      {status.name}
-                    </SelectItem>
-                  ))}
-                </SelectContent>
-              </Select>
-            </div>
-            <div className="flex flex-col">
-              <Label className="text-xs">Created by</Label>
-              <Input
-                disabled
-                defaultValue={`${createdByUser?.firstName} ${createdByUser?.lastName}`}
-              />
-            </div>
-            <div className="flex flex-col">
-              <span className="text-xs text-stone-300">Assigned To</span>
-              <Popover open={open} onOpenChange={setOpen}>
-                <PopoverTrigger asChild>
-                  <Button
-                    variant="outline"
-                    role="combobox"
-                    aria-expanded={open}
-                    className=" justify-between"
-                  >
-                    {value
-                      ? users.find((user) => user.value === value)?.label
-                      : "Select users..."}
-                    <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
-                  </Button>
-                </PopoverTrigger>
-                <PopoverContent className="p-0">
-                  <Command
-                    filter={(value, search) => {
-                      if (
-                        users
-                          .find((user) => user.value === value)
-                          ?.label.toLowerCase()
-                          .includes(search.toLowerCase())
-                      )
-                        return 1;
-                      return 0;
-                    }}
-                  >
-                    <CommandInput placeholder="Select users..." />
-                    <CommandEmpty>No user set</CommandEmpty>
-                    <CommandGroup>
-                      {users.map((user) => (
-                        <CommandItem
-                          key={user.value}
-                          value={user.value}
-                          onSelect={(currentValue) => {
-                            setValue(currentValue);
-                            handleChange(currentValue, "assignedTo");
-                            setOpen(false);
-                          }}
-                        >
-                          <Check
-                            className={cn(
-                              "mr-2 h-4 w-4",
-                              value === user.value ? "opacity-100" : "opacity-0"
-                            )}
-                          />
-                          {user.label}
-                        </CommandItem>
-                      ))}
-                    </CommandGroup>
-                  </Command>
-                </PopoverContent>
-              </Popover>
-            </div>
-          </div>
-          <div className="p-8 bg-stone-900 rounded-xl">
-            <h2 className="text-2xl font-bold mb-4">Audit Logs</h2>
-            <ul className="list-disc list-inside space-y-2">
-              <li>Task assigned to Prateek</li>
-              <li>Task created by Prateek Soni</li>
-            </ul>
-          </div>
+          <Card>
+            <CardHeader>
+              <CardTitle>Details</CardTitle>
+            </CardHeader>
+            <CardContent className="grid grid-cols-2 gap-4">
+              <div className="flex flex-col">
+                <Label className="text-xs" htmlFor="budget">
+                  Budget
+                </Label>
+                <Input
+                  className="appearance-none [-moz-appearance:textfield]"
+                  type="number"
+                  id="budget"
+                  onChange={(e) => handleChange(e.target.value, "budget")}
+                  value={task.budget ?? 0}
+                />
+              </div>
+              <div className="flex flex-col">
+                <Label className="text-xs">Status</Label>
+                <Select
+                  value={task.statusId?.toString()}
+                  onValueChange={(v) => handleChange(v, "statusId")}
+                >
+                  <SelectTrigger className="">
+                    <SelectValue placeholder="Status" />
+                  </SelectTrigger>
+                  <SelectContent>
+                    {props.statuses.map((status) => (
+                      <SelectItem key={status.id} value={status.id.toString()}>
+                        {status.name}
+                      </SelectItem>
+                    ))}
+                  </SelectContent>
+                </Select>
+              </div>
+              <div className="flex flex-col">
+                <Label className="text-xs">Created by</Label>
+                <Input
+                  disabled
+                  defaultValue={`${createdByUser?.firstName} ${createdByUser?.lastName}`}
+                />
+              </div>
+              <div className="flex flex-col">
+                <span className="text-xs text-stone-300">Assigned To</span>
+                <Popover open={open} onOpenChange={setOpen}>
+                  <PopoverTrigger asChild>
+                    <Button
+                      variant="outline"
+                      role="combobox"
+                      aria-expanded={open}
+                      className=" justify-between"
+                    >
+                      {value
+                        ? users.find((user) => user.value === value)?.label
+                        : "Select users..."}
+                      <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+                    </Button>
+                  </PopoverTrigger>
+                  <PopoverContent className="p-0">
+                    <Command
+                      filter={(value, search) => {
+                        if (
+                          users
+                            .find((user) => user.value === value)
+                            ?.label.toLowerCase()
+                            .includes(search.toLowerCase())
+                        )
+                          return 1;
+                        return 0;
+                      }}
+                    >
+                      <CommandInput placeholder="Select users..." />
+                      <CommandEmpty>No user set</CommandEmpty>
+                      <CommandGroup>
+                        {users.map((user) => (
+                          <CommandItem
+                            key={user.value}
+                            value={user.value}
+                            onSelect={(currentValue) => {
+                              setValue(currentValue);
+                              handleChange(currentValue, "assignedTo");
+                              setOpen(false);
+                            }}
+                          >
+                            <Check
+                              className={cn(
+                                "mr-2 h-4 w-4",
+                                value === user.value
+                                  ? "opacity-100"
+                                  : "opacity-0"
+                              )}
+                            />
+                            {user.label}
+                          </CommandItem>
+                        ))}
+                      </CommandGroup>
+                    </Command>
+                  </PopoverContent>
+                </Popover>
+              </div>
+            </CardContent>
+          </Card>
+          <Card>
+            <CardHeader>
+              <CardTitle>Audit Logs</CardTitle>
+            </CardHeader>
+          </Card>
         </div>
       </div>
     </>
--- app/src/components/typography.tsx
+++ app/src/components/typography.tsx
@@ -1,7 +1,9 @@
+import { cn } from "@/lib/utils";
 import React from "react";
 
 interface Props {
   children: React.ReactNode;
+  className?: string;
 }
 
 export function TypographyH1(props: Props) {
@@ -9,5 +11,13 @@ export function TypographyH1(props: Props) {
     <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
       {props.children}
     </h1>
+  );
+}
+
+export function TypographyLead(props: Props) {
+  return (
+    <p className={cn("text-xl text-muted-foreground", props.className)}>
+      {props.children}
+    </p>
   );
 }
