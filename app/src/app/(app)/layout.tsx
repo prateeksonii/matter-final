3465ede2b42cd05f6a53fa174596efd591f45643
prateeksonii <prateek_soni@outlook.com>
2023-12-17 05:19:29 +0530

    wait for user creation

--- app/src/app/(app)/layout.tsx
+++ app/src/app/(app)/layout.tsx
@@ -1,13 +1,34 @@
-import { UserButton } from "@clerk/nextjs";
+import { UserButton, auth } from "@clerk/nextjs";
 import React from "react";
 import Breadcrumb from "./breadcrumb";
 import Link from "next/link";
+import { db } from "@/db/conn";
+import { users } from "@/db/schema";
+import { eq } from "drizzle-orm";
 
-export default function DashboardLayout({
+async function checkUserCreation() {
+  return new Promise((res, rej) => {
+    const interval = setInterval(async () => {
+      const { userId } = auth();
+      const signedInUser = await db
+        .select()
+        .from(users)
+        .where(eq(users.clerkId, userId!));
+
+      if (signedInUser.length > 0) {
+        clearInterval(interval);
+        res(true);
+      }
+    }, 1000);
+  });
+}
+
+export default async function DashboardLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  await checkUserCreation();
   return (
     <div className="min-h-screen">
       <nav className="h-16 bg-stone-900 p-2 flex items-center">
